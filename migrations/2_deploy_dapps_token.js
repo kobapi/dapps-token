77f5b5a6b47ad4d8ae8fbda6049efb943a62a55a
kobayashi <kobayashi@crss-m.jp>
2018-03-26 03:00:46 +0900

    first commit

---
+++ migrations/2_deploy_dapps_token.js
@@ -0,0 +1,8 @@
+var DappsToken = artifacts.require("./DappsToken.sol");
+
+module.exports = function (deployer) {
+    var initialSupply = 1000e18;
+    deployer.deploy(DappsToken, initialSupply, {
+        gas: 2000000
+    });
+}
---
+++ truffle.js
@@ -0,0 +1,25 @@
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "";
+var accessToken = "";
+
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "10" // Match any network id
+    },
+    ropsten: {
+      provider: function(){
+        return new HDWalletProvider(
+            mnemonic,
+            "https://ropsten.infura.io/" + accessToken
+          );
+      },
+      network_id: 3,
+      gas: 500000
+    }
+  }
+};
